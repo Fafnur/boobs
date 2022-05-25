544e81a0ea5a15c18977b71fe4c0c4df4aa5d903
Aleksandr Serenko <fafnur@yandex.ru>
2022-05-25 08:17:49 +0700

    [Web] Add sample using svg

--- packages/web-store/src/app/app.element.ts
+++ packages/web-store/src/app/app.element.ts
@@ -20,19 +20,7 @@ export class AppElement extends HTMLElement {
         <div id="hero" class="rounded">
           <div class="text-container">
             <h2>
-              <svg
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
-                />
-              </svg>
+               <img src="/assets/images/angular-material-logo.svg" alt="">
               <span>You&apos;re up and running</span>
             </h2>
             <a href="#commands"> What&apos;s next? </a>
